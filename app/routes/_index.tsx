65bed6da499e33564b35fe1750c777379ee993d4
Andy Rakauskas <andy.rak@gmail.com>
2023-05-19 09:44:29 -0700

    fix: :bug: fix and force https

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import { Links, useLoaderData } from '@remix-run/react';
-import { json, LinksFunction, LoaderFunction } from '@remix-run/node';
+import { json, LinksFunction, LoaderFunction, redirect } from '@remix-run/node';
 
 import stylesUrl from "~/styles/index.css";
 
@@ -7,13 +7,30 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesUrl },
 ];
 
-export let loader: LoaderFunction = async () => {
+export let loader: LoaderFunction = async ({request}) => {
   const posted_random = Math.random();
   const text = posted_random < 0.8 ? 'YES' : 'NO';
 
   const image_random = Math.floor(Math.random() * 20);
   const path = '/n' + image_random + '.jpg';
 
+  let url = new URL(request.url);
+  let hostname = url.hostname;
+  let proto = request.headers.get("X-Forwarded-Proto") ?? url.protocol;
+
+  url.host =
+    request.headers.get("X-Forwarded-Host") ??
+    request.headers.get("host") ??
+    url.host;
+  url.protocol = "https:";
+
+  if (proto === "http" && hostname !== "localhost") {
+    return redirect(url.toString(), {
+      headers: {
+        "X-Forwarded-Proto": "https",
+      },
+    });
+  }
 
   return json({ path, text });
 }
